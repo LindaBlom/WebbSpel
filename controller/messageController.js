e28e4cea3f74227d133a262ed06f0a243d7b2f4f
Linda Blom <linda.johanna.blom@gmail.com>
2024-07-28 15:06:11 +0200

    minor changers

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,7 +1,17 @@
 
+export const firstMessages = [
+    'Heej! är du upptagen? :D', 
+    'Hallå svara!!!', 
+    'Svara dååå!'
+];
+
+export const firstAnswers = [
+    'Nämen vafan sluta Anna gå därifrån!',
+    'jaa det är ju mycket pengar...',
+    'Ja vad bra! Då kan du bjuda mig på middag sen :)'
+]
 
 
-function addMessage (message) {messages.push(message)};
 
 
 function getMessagesById(response) {}
